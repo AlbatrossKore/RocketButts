5e1a61c08feb87ef778332b3d01d3a9f63338a0b
AlbatrossDev <ceeceebird@gmail.com>
2020-08-18 17:21:13 -0700

    boop~

---
+++ Assets/Scripts/DebugKeys.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugKeys : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.C))
+        {
+            // Stop collisions
+            // Call UI Element to let user know debug key has been activated
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+            // Call UI Element to let user know debug key has been activated 
+        }
+    }
+ 
+    void OnCollisionEnter(Collision other)
+    {
+        
+    }
+    #endif
+}
